4d0c18c64513e7502095216b8dfc2ade3a354bc4
Cleber Lee da Rocha <cleberli.rocha@gmail.com>
2021-10-22 12:28:48 -0300

    User Controller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function AuthRouteAPI(Request $request)
+    {
+        return $request->user();
+    }
+}
